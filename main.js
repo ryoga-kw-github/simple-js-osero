10b458d74350f53b425fd4bbcf1e5478a4686eaf
ryoga_kw <69712737+ryoga-kw-github@users.noreply.github.com>
2021-01-11 23:47:55 +0900

    add core func some codes

--- main.js
+++ main.js
@@ -17,5 +17,22 @@ window.onload = function() {
     let othelloColor = othelloBlack;
     //まだ何のための変数なのかわかんない
 
+    for (let $i=0; $i < $tableElements.length; $i++) {
+    //テーブルのすべてのマスにclick判定をつける
+        
+        //$tableElementsを配列に変換する
+        //変換した配列を利用して、クリックした位置のマスを取得する
+        //putOthello()を実行
+        //changeOrder()を実行
+    }
+
+    function putOthello() {
+        //クリックしたマスに自分の駒を置く関数
+    }
+
+    function changeOrder() {
+        //順番を交代させる関数
+    }
+
 
 };
