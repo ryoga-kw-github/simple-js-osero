4ab45a7fe33c7136bff6048206d69c9d43195dd7
ryoga_kw <69712737+ryoga-kw-github@users.noreply.github.com>
2021-01-12 22:56:04 +0900

    fix some code&comment

--- main.js
+++ main.js
@@ -18,16 +18,24 @@ window.onload = function() {
     //othelloColorにothelloBlack(黒駒●)を代入する
 
     for (let $i=0; $i < $tableElements.length; $i++) {
-    //テーブルのすべてのマスにclick判定をつける
-        
-        //$tableElementsを配列に変換する
-        //変換した配列を利用して、クリックした位置のマスを取得する
-        //putOthello()を実行
-        //changeOrder()を実行
+    //「$i＜全てのマス数」の条件を満たすまで、以下の処理を繰り返す
+
+        $tableElements[$i].addEventListener('click', function(){
+        //マスにclick判定をつける
+
+            let element = [].slice.call($tableElements);
+            //$tableElementsを配列に変換する
+            let index = element.indexOf(this);
+            //変換した配列を利用して、クリックした位置のマスを取得する
+            putOthello(index);
+            //putOthello()を実行
+            changeOrder();
+            //changeOrder()を実行
+        });
     }
 
     function putOthello() {
-        //クリックしたマスに自分の駒を置く関数
+        
     }
 
     function changeOrder() {
