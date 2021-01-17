6bc39f0b90426bfa056c0aeabd6102807925f60b
ryoga_kw <69712737+ryoga-kw-github@users.noreply.github.com>
2021-01-17 19:53:09 +0900

    fix table id

--- index.php
+++ index.php
@@ -45,84 +45,84 @@
         <table class="table">
             <tbody>
                 <tr>
-                    <td>11</td>
-                    <td>12</td>
-                    <td>13</td>
-                    <td>14</td>
-                    <td>15</td>
-                    <td>16</td>
-                    <td>17</td>
-                    <td>18</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
                 </tr>
                 <tr>
-                    <td>21</td>
-                    <td>22</td>
-                    <td>23</td>
-                    <td>24</td>
-                    <td>25</td>
-                    <td>26</td>
-                    <td>27</td>
-                    <td>28</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
                 </tr>
                 <tr>
-                    <td>31</td>
-                    <td>32</td>
-                    <td>33</td>
-                    <td>34</td>
-                    <td>35</td>
-                    <td>36</td>
-                    <td>37</td>
-                    <td>38</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
                 </tr>
                 <tr>
-                    <td>41</td>
-                    <td>42</td>
-                    <td>43</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
                     <td>○</td>
                     <td>●</td>
-                    <td>46</td>
-                    <td>47</td>
-                    <td>48</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
                 </tr>
                 <tr>
-                    <td>51</td>
-                    <td>52</td>
-                    <td>53</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
                     <td>●</td>
                     <td>○</td>
-                    <td>56</td>
-                    <td>57</td>
-                    <td>58</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
                 </tr>
                 <tr>
-                    <td>61</td>
-                    <td>62</td>
-                    <td>63</td>
-                    <td>64</td>
-                    <td>65</td>
-                    <td>66</td>
-                    <td>67</td>
-                    <td>68</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
                 </tr>
                 <tr>
-                    <td>71</td>
-                    <td>72</td>
-                    <td>73</td>
-                    <td>74</td>
-                    <td>75</td>
-                    <td>76</td>
-                    <td>77</td>
-                    <td>78</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
                 </tr>
                 <tr>
-                    <td>81</td>
-                    <td>82</td>
-                    <td>83</td>
-                    <td>84</td>
-                    <td>85</td>
-                    <td>86</td>
-                    <td>87</td>
-                    <td>88</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
                 </tr>
             </tbody>
         </table>
